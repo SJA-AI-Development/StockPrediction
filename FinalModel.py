97f6f31ca79369479f73e5f58bb021ab50a77027
Arkz734 <diazxc1010@gmail.com>
2022-05-17 22:35:56 +0900

    trash commit

---
+++ FinalModel.py
@@ -0,0 +1,16 @@
+import FinanceDataReader as fdr
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import redis
+
+df_krx = fdr.StockListing('KRX')
+df_naver = fdr.DataReader('005930', '2018')
+
+target = df_naver['Close']
+pred = df_naver[['Open']]
+
+x_train, x_test, y_train, y_test = train_test_split(pred, target, test_size = 0.3, random_state = 25, shuffle=False)
+
+randomforest = RandomForestRegressor(random_state=0)
+randomforest.fit(x_train, y_train)
+y_pred_rf = randomforest.predict(x_test)
